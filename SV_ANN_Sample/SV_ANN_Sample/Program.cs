a32caa7280a4494ebfa49dd218711f186370912a
porestis <pav.orestis@gmail.com>
2014-09-04 15:59:26 +0300

    Base Commit

---
+++ SV_ANN_Sample/SV_ANN_Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SV_ANN_Sample {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
